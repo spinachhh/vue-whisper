efa61255ef75cd9d54be1846b7b049ca5ffd02b6
zzw <37894941+spinachhh@users.noreply.github.com>
2019-09-29 16:28:37 +0800

    Add files via upload

---
+++ dist/precache-manifest.e10a6973a6d847ede8d506c112db4541.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a28c23f7bf7e53dffc",
+    "url": "static/css/app.401add3f.css"
+  },
+  {
+    "revision": "24a28c23f7bf7e53dffc",
+    "url": "static/js/app.a4762c28.js"
+  },
+  {
+    "revision": "a2af38158ca1847149c7",
+    "url": "static/css/chunk-3cb88da2.c4f08d0a.css"
+  },
+  {
+    "revision": "a2af38158ca1847149c7",
+    "url": "static/js/chunk-3cb88da2.75c5b9d0.js"
+  },
+  {
+    "revision": "80d40ed802cc17b77566",
+    "url": "static/css/chunk-72022a24.36c176d6.css"
+  },
+  {
+    "revision": "80d40ed802cc17b77566",
+    "url": "static/js/chunk-72022a24.c466250c.js"
+  },
+  {
+    "revision": "5bda67ed2b7a74eca401",
+    "url": "static/css/chunk-eb980726.a60828c8.css"
+  },
+  {
+    "revision": "5bda67ed2b7a74eca401",
+    "url": "static/js/chunk-eb980726.db2ed2f8.js"
+  },
+  {
+    "revision": "b918106eca425bccdffe",
+    "url": "static/css/chunk-vendors.779f7d1d.css"
+  },
+  {
+    "revision": "b918106eca425bccdffe",
+    "url": "static/js/chunk-vendors.c402ef54.js"
+  },
+  {
+    "revision": "fd928753abac5d93bbaa4b17ddc1024c",
+    "url": "static/img/pause.fd928753.png"
+  },
+  {
+    "revision": "2f765cc1991a5a532af4491631e5717c",
+    "url": "static/img/iconfont.2f765cc1.svg"
+  },
+  {
+    "revision": "5ef05db303f8461c2452373c13159bb2",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "static/.gitkeep"
+  },
+  {
+    "revision": "0ce6af34c9612975577add530e4ab819",
+    "url": "static/amfe-flexible.js"
+  }
+];
